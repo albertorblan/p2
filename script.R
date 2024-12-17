8e86251f0b2d525f931af397eb0abb6f9913d3b3
Alberto <albertorblan06@gmail.com>
2024-12-17 12:10:18 +0100

    17.12.24

---
+++ script.R
@@ -0,0 +1,15 @@
+rm(list=ls(all=TRUE))
+getwd()
+setwd("/.../...") 
+
+#############################Cargar y explorar datos############################
+datos = read.table("datos_grupo_1.txt", sep="\t", header=TRUE)
+datos$Tipo.de.proyecto = as.factor(datos$Tipo.de.proyecto)
+
+dim(datos)
+str(datos)
+summary(datos)
+names(datos)
+
+
+################Clacular e interpretar intervalos de confianza##################
